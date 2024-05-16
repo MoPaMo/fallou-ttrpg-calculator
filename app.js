d4d10819aeb904e70bf38fbff00c71a5608f2a4d
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-05-16 20:07:25 +0200

    fix modifiers

--- app.js
+++ app.js
@@ -61,4 +61,45 @@ new Vue({
       deep: true,
     },
   },
+  computed: {
+    remainingPoints() {
+      let usedPoints = this.stats.reduce((acc, stat) => acc + stat.value, 0);
+      return this.totalPoints - usedPoints;
+    },
+  },
+  created() {
+    this.stats.forEach((stat) => {
+      this.$set(stat, "modifier", this.calculateModifier(stat.value));
+    });
+  },
+  methods: {
+    updateValue(stat, event) {
+      let newValue = parseInt(event.target.value);
+      if (!isNaN(newValue) && newValue >= 1 && newValue <= 10) {
+        stat.value = newValue;
+        stat.modifier = this.calculateModifier(newValue);
+        this.validatePoints();
+      }
+    },
+    calculateModifier(value) {
+      return value - 5;
+    },
+    validatePoints() {
+      let totalUsedPoints = this.stats.reduce(
+        (acc, stat) => acc + stat.value,
+        0
+      );
+      if (totalUsedPoints > this.totalPoints) {
+        let excess = totalUsedPoints - this.totalPoints;
+        for (let stat of this.stats) {
+          if (stat.value > 1 && excess > 0) {
+            let reduction = Math.min(stat.value - 1, excess);
+            stat.value -= reduction;
+            excess -= reduction;
+            stat.modifier = this.calculateModifier(stat.value);
+          }
+        }
+      }
+    },
+  },
 });
